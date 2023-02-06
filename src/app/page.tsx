a3b99e1ec5e847f68c7a8b4ad0b6047ca0d0ba3e
you <you@yunoMacBook-Pro.local>
2023-02-06 20:21:48 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from './page.module.css'
+// const inter = Inter({ subsets: ['latin'] })
+
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hello</h1>
+      <div className='flex items-center justify-between'>
+        <button
+          className='px-4 py-2 rounded-lg animate-in fade-in zoom-in bg-lime-400'
+          onClick={() => signIn()}
+        >
+          sign in
+        </button>
+        <button className='px-4 py-2 rounded-lg bg-lime-400' onClick={() => signOut()}>
+          sign out
+        </button>
+      </div>
+    </main>
+  )
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,73 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { db } from '@/lib/db'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db as any),
+  session: {
+    strategy: 'jwt',
+  },
+  // pages: {
+  //   signIn: '/login',
+  // },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token && session.user) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        token.id = user.id
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+  },
+}
